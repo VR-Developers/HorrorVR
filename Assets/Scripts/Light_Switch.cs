03db877df3c7fe74f7f59ca46f5e6894c3a2a579
Remi Storms <ultimate.spidey.18@gmail.com>
2016-10-02 00:53:45 -0500

    - Added Google VR SDK - Added main projkect folders - Added test room with crates - Test lighting and shadows (Real time and baked)

---
+++ Assets/Scripts/Light_Switch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Light_Switch : MonoBehaviour {
+
+	public GameObject flashlight;
+	public bool flashlight_state = false;
+	// Use this for initialization
+	void Start () 
+	{
+		flashlight.SetActive (false);
+		flashlight_state = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonUp(0)) // OR CARDBOARD TRIGGER
+		{
+			Debug.Log ("Toggle Light");
+			flashlight_state = !flashlight_state;
+			flashlight.SetActive (flashlight_state);
+		}
+	}
+}
